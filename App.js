7baea022e9fe0c4398e8302b05bf723393a2dcce
Akshay <akshay.kamble@copods.co>
2023-11-23 15:50:42 +0530

    intial

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        data.map((post) => {
+          return (
+            <View>
+              <Text style={styles.title}>{post.title}</Text>
+              <Text style={{ backgroundColor: "red" }}>{post.body}</Text>
+            </View>
+          );
+        })
+      )}
     </View>
   );
 }
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
